6ec81f2e201a0384449eb5edd4c07adfa10e910a
yaoxulin <yaoxulin@apipost.cn>
2024-04-30 15:43:31 +0800

    feat: first

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import BundleAnalyzer from '@next/bundle-analyzer';
 
-export default nextConfig;
+const withBundleAnalyzer = BundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true', // 当环境为ANALYZE为true是开启
+});
+
+const nextConfig = {
+  sassOptions: {
+    additionalData: '@import "@/styles/index.scss";',
+  },
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.test.com',
+      },
+    ],
+  },
+};
+
+export default withBundleAnalyzer(nextConfig);
